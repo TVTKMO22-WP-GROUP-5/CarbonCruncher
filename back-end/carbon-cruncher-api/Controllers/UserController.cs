be6c3d86e8bc388bde97cd3d8e9fc773005b5775
Esa Salminen <esa_salminen@hotmail.com>
2023-04-02 12:06:04 +0300

    added: register and login routes to usercontroller

--- back-end/carbon-cruncher-api/Controllers/UserController.cs
+++ back-end/carbon-cruncher-api/Controllers/UserController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using carbon_cruncher_api.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
 
 namespace carbon_cruncher_api.Controllers
 {
@@ -7,49 +9,117 @@ namespace carbon_cruncher_api.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        // POST: api/user/register
-        [HttpPost("register")]
-        public IEnumerable<string> Register([FromBody] string value)
+        // Database context
+        private readonly CarbonCruncherContext _context;
+
+        // Database context set with dependency injection
+        public UserController(CarbonCruncherContext context)
         {
-            return new string[] { "value1", "value2" };
+            _context = context;
         }
 
-        // POST: api/user/login
-        [HttpPost("login")]
-        public IEnumerable<string> Login([FromBody] string value)
+        /// <summary>
+        /// Register a user
+        /// </summary>
+        /// <param name="user">User object with user nick and password.</param>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     Post /api/user
+        ///     {
+        ///        "usernick":"testguy",
+        ///        "userpassword":"R4nd0mP4ssw0rd!"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="200">User registered succesfully</response>
+        /// <response code="400">Error while registering user</response>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Consumes("application/json")]
+        [Produces("text/plain")]
+        [HttpPost]
+        [Route("register")]
+        public ActionResult<string> Register([FromBody] VisuRegLoginUser user)
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                VisuUser regUser = new VisuUser() { UserNick = user.UserNick, UserPassHash = "jfo93jf39" };   
+                
+                _context.VisuUsers.Add(regUser);
+                _context.SaveChanges();  
+                
+                return Ok($"User {regUser.UserNick} registered succesfully with id {regUser.Id}");
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// Login a user
+        /// </summary>
+        /// <param name="user">User object with user nick and password.</param>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     Post /api/login
+        ///     {
+        ///        "usernick":"testguy",
+        ///        "userpassword":"R4nd0mP4ssw0rd!"
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="200">User logged in succesfully</response>
+        /// <response code="400">Error while user login</response>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Consumes("application/json")]
+        [Produces("text/plain")]
+        [HttpPost]
+        [Route("login")]
+        public string Login([FromBody] VisuRegLoginUser user)
+        {
+            return "token";
         }
 
         // DELETE api/user/:id
-        [HttpDelete("{id}")]
+        [HttpDelete]
+        [Route("{id}")]
         public void Delete(int id)
         {
         }
 
         // GET: api/user/visualization
-        [HttpGet("visualization")]
+        [HttpGet]
+        [Route("visualization")]
         public IEnumerable<string> GetVisualization()
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET: api/user/visualization/:stringId
-        [HttpGet("visualization/{stringId}")]
+        [HttpGet]
+        [Route("visualization/{stringId}")]
         public IEnumerable<string> GetSingleVisualization(string stringId)
         {
             return new string[] { "value1", "value2" };
         }
 
         // POST: api/user/visualization
-        [HttpPost("visualization")]
+        [HttpPost]
+        [Route("visualization")]
         public IEnumerable<string> PostVisualization([FromBody] string value)
         {
             return new string[] { "value1", "value2" };
         }
 
         // DELETE api/visualization/:id
-        [HttpDelete("visualization/{id}")]
+        [HttpDelete]
+        [Route("visualization/{id}")]
         public void DeleteVisualization(int id)
         {
         }
---
+++ back-end/carbon-cruncher-api/Models/VisuRegLoginUser.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace carbon_cruncher_api.Models
+{
+    public class VisuRegLoginUser
+    {
+        [Required]
+        [StringLength(20,MinimumLength = 3)]
+        public string UserNick { get; set; } = string.Empty;
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string UserPassword { get; set; } = string.Empty;
+    }
+}
