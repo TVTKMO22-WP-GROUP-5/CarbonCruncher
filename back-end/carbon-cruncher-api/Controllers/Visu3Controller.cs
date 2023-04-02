6d6874dc1d82f366bf6d9692235252424795dfce
Kaisa <kkangas@protonmail.com>
2023-04-02 18:02:34 +0200

    Visu3controller added

--- back-end/carbon-cruncher-api/Controllers/Visu3Controller.cs
+++ back-end/carbon-cruncher-api/Controllers/Visu3Controller.cs
@@ -1,31 +1,76 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using carbon_cruncher_api.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace carbon_cruncher_api.Controllers
 {
     [Route("api/visu3")]
     [ApiController]
     public class Visu3Controller : ControllerBase
+
     {
-        // GET: api/visu3/Global
-        [HttpGet("global")]
-        public IEnumerable<string> Global()
+        // Database context 
+        private readonly CarbonCruncherContext _context;
+
+        // Database context set with dependency injection
+        public Visu3Controller(CarbonCruncherContext context)
         {
-            return new string[] { "value1", "value2" };
+            _context = context;
+        }
+
+        //GET: api/visu3/Global
+        /// <summary>
+        /// Get evolution of global temperature over the past two million years.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///     Get/api/visu3/global
+        /// </remarks>
+        /// <response code="200">
+        /// Returns evolution of global temperature over the past two million years.
+        /// </response>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Visu3Global>), StatusCodes.Status200OK)]
+        [Produces("application/json")]
+
+        //[HttpGet("global")]
+        [HttpGet]
+        [Route("global")]
+        public IEnumerable<Visu3Global> Global()
+        {
+            return _context.Visu3Global.ToList();
         }
 
         // GET: api/visu3/CO2
-        [HttpGet("co2")]
+        /*[HttpGet("co2")]
         public IEnumerable<string> CO2()
         {
             return new string[] { "value1", "value2" };
         }
+        */
 
         // GET: api/visu3/Event
-        [HttpGet("event")]
-        public IEnumerable<string> Events()
+        /// <summary>
+        /// Get milestones in human evolution and culture events over the past two million years.
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///     Get/api/visu3/event
+        /// </remarks>
+        /// <response code="200">
+        /// Returns milestones in human evolution and culture events over the past two million years.
+        /// </response>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Visu3Event>), StatusCodes.Status200OK)]
+        [Produces("application/json")]
+
+        //[HttpGet("event")]
+        [HttpGet]
+        [Route("event")]
+        public IEnumerable<Visu3Event> Events()
         {
-            return new string[] { "value1", "value2" };
+            return _context.Visu3Event.ToList();
         }
     }
 }
