520f27d7a14c57e820b059c8e06363fea878550a
Esa Salminen <t1saes01@students.oamk.fi>
2023-04-09 16:46:10 +0300

    added: react application structure

--- front-end/carbon-cruncher/src/App.js
+++ front-end/carbon-cruncher/src/App.js
@@ -1,9 +1,19 @@
+import React from "react";
+import "./App.css";
+import AuthProvider from "./components/AuthProvider";
+import { Routes, Route } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage";
+import { MainPage } from "./pages/MainPage";
 
 function App() {
   return (
-    <div>
-      <h1>Hello Carbon Cruncher!</h1>
-    </div>
+    <AuthProvider>
+      <Routes>
+        <Route index element={<LoginPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="main" element={<MainPage />} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
---
+++ front-end/carbon-cruncher/src/pages/LoginPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+export const LoginPage = () => {
+  return (
+    <div className="loginPage">
+      <h1>Loginpage (public)</h1>
+    </div>
+  );
+};
