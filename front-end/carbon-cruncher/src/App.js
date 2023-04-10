244641513a7dfc73073009009ec96508856ea157
Kaisa <kkangas@protonmail.com>
2023-04-10 21:43:29 +0300

    added visu2 component

--- front-end/carbon-cruncher/src/App.js
+++ front-end/carbon-cruncher/src/App.js
@@ -1,9 +1,13 @@
+import Visu2 from "./components/Visu2";
 
 function App() {
   return (
-    <div>
+    /*<div>
       <h1>Hello Carbon Cruncher!</h1>
-    </div>
+
+    </div>*/
+    <Visu2 />
+    
   );
 }
 
