0e10d9a3bd8112bf95e55de6d450ba9270fc8272
anssikulotie <anssi.kulotie@hotmail.com>
2023-04-11 15:54:22 +0300

    visu1 päivitys

--- front-end/carbon-cruncher/src/components/Visu1.js
+++ front-end/carbon-cruncher/src/components/Visu1.js
@@ -1,103 +1,96 @@
-import React, { useState, useEffect, useRef } from 'react';
-import Chart from 'chart.js';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Line } from "react-chartjs-2";
 
-const Visualization1 = () => {
-  const [monthly, setMonthly] = useState(false);
-  const [globalData, setGlobalData] = useState([]);
-  const [nhData, setNhData] = useState([]);
-  const [shData, setShData] = useState([]);
-  const [years, setYears] = useState([]);
+const Visu1 = () => {
+    // State variables for data, chart data, and view selection
+    const [data, setData] = useState([]);
+    const [chartData, setChartData] = useState({});
+    const [isAnnual, setIsAnnual] = useState(true);
 
-  const chartRef = useRef(null);
+    // Fetch data from the API based on the view selection
+    useEffect(() => {
+        const fetchData = async () => {
+            const url = isAnnual
+                ? "https://carbon-cruncher.azurewebsites.net/api/visu1/annual"
+                : "https://carbon-cruncher.azurewebsites.net/api/visu1/monthly";
+            const result = await axios(url);
+            setData(result.data);
+        };
+        fetchData();
+    }, [isAnnual]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const url = `https://carbon-cruncher.azurewebsites.net/api/visu1/${
-        monthly ? 'monthly' : 'annual'
-      }`;
-      const response = await fetch(url);
-      const data = await response.json();
+    // Update the chart data based on the fetched data and view selection
+    useEffect(() => {
+        const { global, northern, southern } = data;
 
-      setGlobalData(data.global);
-      setNhData(data.nh);
-      setShData(data.sh);
-      setYears(data.years);
+        if (global && northern && southern) {
+            const chartData = {
+                labels: isAnnual
+                    ? global.map((d) => d.year)
+                    : global[0].monthly.map((d) => d.date),
+                datasets: [
+                    {
+                        label: "Global",
+                        data: isAnnual
+                            ? global.map((d) => d.mean)
+                            : global[0].monthly.map((d) => d.anomaly),
+                        borderColor: "red",
+                        fill: false,
+                    },
+                    {
+                        label: "Northern Hemisphere",
+                        data: isAnnual
+                            ? northern.map((d) => d.mean)
+                            : northern[0].monthly.map((d) => d.anomaly),
+                        borderColor: "blue",
+                        fill: false,
+                    },
+                    {
+                        label: "Southern Hemisphere",
+                        data: isAnnual
+                            ? southern.map((d) => d.mean)
+                            : southern[0].monthly.map((d) => d.anomaly),
+                        borderColor: "green",
+                        fill: false,
+                    },
+                ],
+            };
+            setChartData(chartData);
+        }
+    }, [data, isAnnual]);
+
+    // Update the view selection based on user input
+    const handleChange = (event) => {
+        setIsAnnual(event.target.value === "annual");
     };
 
-    fetchData();
-  }, [monthly]);
-
-  useEffect(() => {
-    const ctx = chartRef.current.getContext('2d');
-    new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: years,
-        datasets: [
-          {
-            label: 'Global',
-            data: globalData,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
-            borderWidth: 1,
-            fill: false,
-          },
-          {
-            label: 'Northern Hemisphere',
-            data: nhData,
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgba(54, 162, 235, 1)',
-            borderWidth: 1,
-            fill: false,
-          },
-          {
-            label: 'Southern Hemisphere',
-            data: shData,
-            backgroundColor: 'rgba(255, 206, 86, 0.2)',
-            borderColor: 'rgba(255, 206, 86, 1)',
-            borderWidth: 1,
-            fill: false,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          xAxes: [
-            {
-              scaleLabel: {
-                display: true,
-                labelString: 'Year',
-              },
-            },
-          ],
-          yAxes: [
-            {
-              scaleLabel: {
-                display: true,
-                labelString: 'Temperature Anomaly (°C)',
-              },
-            },
-          ],
-        },
-      },
-    });
-  }, [globalData, nhData, shData, years]);
-
-  return (
-    <div>
-      <canvas ref={chartRef} />
-      <div>
-        <button onClick={() => setMonthly(true)}>Monthly</button>
-        <button onClick={() => setMonthly(false)}>Annual</button>
-      </div>
-      <p>
-        Data source:{' '}
-        <a href="https://carbon-cruncher.azurewebsites.net/api/visu1/annual">
-          https://carbon-cruncher.azurewebsites.net/api/visu1/annual
-        </a>
-      </p>
-    </div>
-  );
+    // Render the chart and view selection controls
+    return (
+        <div>
+            <div>
+                <input
+                    type="radio"
+                    id="annual"
+                    name="view"
+                    value="annual"
+                    checked={isAnnual}
+                    onChange={handleChange}
+                />
+                <label htmlFor="annual">Annual</label>
+                <input
+                    type="radio"
+                    id="monthly"
+                    name="view"
+                    value="monthly"
+                    checked={!isAnnual}
+                    onChange={handleChange}
+                />
+                <label htmlFor="monthly">Monthly</label>
+            </div>
+            <Line data={chartData} />
+        </div>
+    );
 };
 
-export default Visualization1;
+export default Visu1;
