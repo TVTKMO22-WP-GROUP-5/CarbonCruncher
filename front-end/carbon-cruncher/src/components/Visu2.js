75e9729269b304b8b886b3e4b33b4f6b95f9c659
Kaisa <kkangas@protonmail.com>
2023-04-13 20:58:22 +0300

    update visu2 component in progress

--- front-end/carbon-cruncher/src/components/Visu2.js
+++ front-end/carbon-cruncher/src/components/Visu2.js
@@ -1,5 +1,5 @@
 import React from "react";
-//import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Line } from "react-chartjs-2";
 import {
     Chart as ChartJS,
@@ -22,98 +22,7 @@ ChartJS.register(
     Legend,
 )
 
-//const ctx = document.getElementById("Visu2").getContext("2d");
-
-const annual_url = "https://carbon-cruncher.azurewebsites.net/api/visu2/annual";
-
-async function getAnnual() {
-    const response = await fetch (annual_url);
-    const data = await response.json();
-    console.log(data);
-}
-getAnnual();
-
-//const [co2, setCo2] = useState ('CO2');
-//const [icecore, setIcecore] = useState ('Icecore');
-
-//const icecore1 = icecore.filter(data = data.icecore_id == 1);
-//const icecore2 = icecore.filter(data = data.icecore_id == 2);
-//const icecore3 = icecore.filter(data = data.icecore_id == 3);
-
-//const labels = [1958, 1963, 1968, 1973, 1978, 1983, 1988, 1993, 1998, 2003, 2008, 2013, 2018, 2023]
-
-
-const data = {
-    //labels: labels,   
-    datasets: [
-
-        {
-            label: "CO2 Annual",
-            //data: [320, 330, 340, 350, 380, 390, 420], //testing...
-            data: getAnnual,
-            borderColor: "rgb(0, 0, 0)",
-            backgroundColor: "rgb(0, 0, 0)",
-            pointRadius: 0,
-            parsing: {
-                xAxisValue: "Year",
-                yAxisValue: "Mean",
-            }
-        },
-
-        /*{
-            label: "CO2 Monthly",
-            data: [1, 6, 280, 425], //testing...
-            //data: visu2.Monthly,
-            borderColor: "rgb(240, 150, 15)",
-            backgroundColor: "rgb(240, 150, 15)",
-            pointRadius: 0,
-            parsing: {
-                xAxisValue: "Datetime Time",
-                yAxisValue: "Average",
-            },
-        },
-
-        {
-            label: "Ice Core 1",
-            data: [20, 70, 80], //testing...
-            //data: visu2.IceCore1,
-            borderColor: "rgb(160, 60, 240)",
-            backgroundColor: "rgb(160, 60, 240)",
-            pointRadius: 0,
-            parsing: {
-                xAxisValue: "air_age",
-                yAxisValue: "co2_ppm",
-            }
-        },
-
-        {
-            label: "Ice Core 2",
-            data: [25, 30, 50], //testing...
-            //data: visu2.IceCore2,
-            borderColor: "rgb(250, 100, 220)",
-            backgroundColor: "rgb(250, 100, 220)",
-            pointRadius: 0,
-            parsing: {
-                xAxisValue: "air_age",
-                yAxisValue: "co2_ppm",
-            }
-        },
-
-        {
-            label: "Ice Core 3",
-            data: [45, 60, 110], //testing...
-            //data: visu2.IceCore3,
-            borderColor: "rgb(230, 15, 15)",
-            backgroundColor: "rgb(230 , 15, 15)",
-            pointRadius: 0,
-            parsing: {
-                xAxisValue: "air_age",
-                yAxisValue: "co2_ppm",
-            }
-        }*/
-        
-    ]
-}
+const labels = [1958, 1963, 1968, 1973, 1978, 1983, 1988, 1993, 1998, 2003, 2008, 2013, 2018, 2023]
 
 const options = {
     responsive: true,
@@ -142,6 +51,168 @@ const options = {
 }
 
 const Visu2 = () => {
+    const [data, setData] = useState({
+        labels: [1958, 1963, 1968, 1973, 1978, 1983, 1988, 1993, 1998, 2003, 2008, 2013, 2018, 2023],
+        datasets: [
+    
+            {
+                label: "CO2 Annual",
+                data: [], 
+                borderColor: "rgb(0, 0, 0)",
+                backgroundColor: "rgb(0, 0, 0)",
+                pointRadius: 0,
+                parsing: {
+                    xAxisValue: "Year",
+                    yAxisValue: "Mean",
+                }
+            },
+    
+            {
+                label: "CO2 Monthly",
+                data: [],
+                borderColor: "rgb(240, 150, 15)",
+                backgroundColor: "rgb(240, 150, 15)",
+                pointRadius: 0,
+                parsing: {
+                    xAxisValue: "Time",
+                    yAxisValue: "Average",
+                },
+            },
+    
+            {
+                label: "Ice Core 1",
+                data: [],
+                borderColor: "rgb(160, 60, 240)",
+                backgroundColor: "rgb(160, 60, 240)",
+                pointRadius: 0,
+                parsing: {
+                    xAxisValue: "air_age",
+                    yAxisValue: "co2_ppm",
+                }
+            },
+    
+            {
+                label: "Ice Core 2",
+                data: [], 
+                borderColor: "rgb(250, 100, 220)",
+                backgroundColor: "rgb(250, 100, 220)",
+                pointRadius: 0,
+                parsing: {
+                    xAxisValue: "air_age",
+                    yAxisValue: "co2_ppm",
+                }
+            },
+    
+            {
+                label: "Ice Core 3",
+                data: [],
+                borderColor: "rgb(230, 15, 15)",
+                backgroundColor: "rgb(230 , 15, 15)",
+                pointRadius: 0,
+                parsing: {
+                    xAxisValue: "air_age",
+                    yAxisValue: "co2_ppm",
+                }
+            }
+            
+        ]
+    }
+    );
+    useEffect(() => {
+        const fetchData=async()=> {
+            const url = "https://carbon-cruncher.azurewebsites.net/api/visu2/annual";
+            const labelSet = [];
+            const dataSet1 = [];
+            const dataSet2 = [];
+        await fetch(url).then((data)=> {
+            console.log("Annual data", data)
+            const res = data.json();
+            return res
+        }).then(()=> {
+            console.log("result",res)
+
+
+
+            
+        }).catch(e => {
+            console.log("error", e)
+        })  
+        }
+        fetchData();
+    },[])
+
+    setdata = {
+        labels: labels,   
+        datasets: [
+    
+            {
+                label: "CO2 Annual",
+                data: [320, 330, 340, 350, 380, 390, 420], //testing...
+                //data: getAnnual,
+                borderColor: "rgb(0, 0, 0)",
+                backgroundColor: "rgb(0, 0, 0)",
+                pointRadius: 0,
+                parsing: {
+                    xAxisValue: "Year",
+                    yAxisValue: "Mean",
+                }
+            },
+    
+            {
+                label: "CO2 Monthly",
+                data: [1, 6, 280, 425], //testing...
+                //data: getMonthly,
+                borderColor: "rgb(240, 150, 15)",
+                backgroundColor: "rgb(240, 150, 15)",
+                pointRadius: 0,
+                parsing: {
+                    xAxisValue: "Time",
+                    yAxisValue: "Average",
+                },
+            },
+    
+            {
+                label: "Ice Core 1",
+                data: [20, 70, 80], //testing...
+                //data: visu2.IceCore1,
+                borderColor: "rgb(160, 60, 240)",
+                backgroundColor: "rgb(160, 60, 240)",
+                pointRadius: 0,
+                parsing: {
+                    xAxisValue: "air_age",
+                    yAxisValue: "co2_ppm",
+                }
+            },
+    
+            {
+                label: "Ice Core 2",
+                data: [25, 30, 50], //testing...
+                //data: visu2.IceCore2,
+                borderColor: "rgb(250, 100, 220)",
+                backgroundColor: "rgb(250, 100, 220)",
+                pointRadius: 0,
+                parsing: {
+                    xAxisValue: "air_age",
+                    yAxisValue: "co2_ppm",
+                }
+            },
+    
+            {
+                label: "Ice Core 3",
+                data: [45, 60, 110], //testing...
+                //data: visu2.IceCore3,
+                borderColor: "rgb(230, 15, 15)",
+                backgroundColor: "rgb(230 , 15, 15)",
+                pointRadius: 0,
+                parsing: {
+                    xAxisValue: "air_age",
+                    yAxisValue: "co2_ppm",
+                }
+            }
+            
+        ]
+    }
+
     return (
     <div style={{width: "1000px", height: "500px"}}>
      <Line data={data} options={options}></Line>;
