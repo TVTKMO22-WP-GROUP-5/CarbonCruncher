48c77d522fe9e36bcd197317135f40d2dba30f5d
Kaisa <kkangas@protonmail.com>
2023-04-16 20:00:21 +0300

    added updates visu2 component

--- front-end/carbon-cruncher/src/components/Visu2.js
+++ front-end/carbon-cruncher/src/components/Visu2.js
@@ -23,6 +23,7 @@ ChartJS.register(
 )
 
 const labels = Array.from(Array(2023+1).keys()).slice(1958);
+const labels2 = Array.from(Array(1966+1).keys()).slice(1006);
 
 const options = {
     responsive: true,
@@ -50,8 +51,10 @@ const options = {
     
 }
 
-const Visu2 = () => {
-    const [data, setData] = useState({
+//const ctx = document.getElementById("visu2");
+
+export const Visu2 = () => {
+    const [annualCo2, setAnnualCo2] = useState ({
         labels: labels,
         datasets: [
     
@@ -59,49 +62,64 @@ const Visu2 = () => {
                 label: "CO2 Annual",
                 data: [], 
             },
-    
-            /*{
+        ]
+    });
+
+    const [monthlyCo2, setMonthlyCo2] = useState ({
+        labels: labels,
+        datasets: [
+            {
                 label: "CO2 Monthly",
                 data: [],
             },
-    
-            {
-                label: "Ice Core 1",
-                data: [],
-            },
-    
-            {
-                label: "Ice Core 2",
-                data: [], 
-    
-            {
-                label: "Ice Core 3",
-                data: [],
-            }*/
-            
         ]
     });
+    
+    const [icecore1, setIcecore1] = useState ({
+        labels: labels2,
+        datasets: [
+            {
+            label: "Ice Core 1",
+            data: [],
+            },
+        ]
+    });
+
+    const [icecore2, setIcecore2] = useState ({
+        labels: labels2,
+        datasets: [
+            {
+            label: "Ice Core 2",
+            data: [],
+            },
+        ]
+    });
+
+    const [icecore3, setIcecore3] = useState ({
+        labels: labels2,
+        datasets: [
+            {
+            label: "Ice Core 3",
+            data: [],
+            },
+        ]
+    });
+
     useEffect(() => {
         const fetchData=async()=> {
             const url_1 = "https://carbon-cruncher.azurewebsites.net/api/visu2/annual";
-            //const url_2 = "https://carbon-cruncher.azurewebsites.net/api/visu2/monthly";
-            //const labelSet = [];
             const dataSet1 = [];
-            //const dataSet2 = [];
-        await fetch(url_1).then((data)=> {
-            console.log("Annual data", data)
-            const res = data.json();
+        await fetch(url_1).then((annualCo2)=> {
+            console.log("Annual data", annualCo2)
+            const res = annualCo2.json();
             return res
         }).then((res)=> {
             console.log("result",res)
             for (const val of res){
                 dataSet1.push(val.mean);
-                //labelSet.push(val.year);
-
         }
-
-        
-        setData ({
+       
+        setAnnualCo2 ({
             labels: labels,   
             datasets: [
             
@@ -116,58 +134,6 @@ const Visu2 = () => {
                         yAxisValue: "Mean",
                     }
                 },
-            
-                /*{
-                    label: "CO2 Monthly",
-                    //data: [1, 6, 280, 425], //testing...
-                    data:dataSet1,
-                    borderColor: "rgb(240, 150, 15)",
-                    backgroundColor: "rgb(240, 150, 15)",
-                    pointRadius: 0,
-                    parsing: {
-                        xAxisValue: "Time",
-                        yAxisValue: "Average",
-                    },
-                },
-            
-                {
-                    label: "Ice Core 1",
-                    //data: [20, 70, 80], //testing...
-                    data:dataSet1,
-                    borderColor: "rgb(160, 60, 240)",
-                    backgroundColor: "rgb(160, 60, 240)",
-                    pointRadius: 0,
-                    parsing: {
-                        xAxisValue: "air_age",
-                        yAxisValue: "co2_ppm",
-                    }
-                },
-            
-                {
-                    label: "Ice Core 2",
-                    //data: [25, 30, 50], //testing...
-                    data:dataSet1,
-                    borderColor: "rgb(250, 100, 220)",
-                    backgroundColor: "rgb(250, 100, 220)",
-                    pointRadius: 0,
-                    parsing: {
-                        xAxisValue: "air_age",
-                        yAxisValue: "co2_ppm",
-                    }
-                },
-            
-                {
-                    label: "Ice Core 3",
-                    //data: [45, 60, 110], //testing...
-                    data:dataSet1,
-                    borderColor: "rgb(230, 15, 15)",
-                    backgroundColor: "rgb(230 , 15, 15)",
-                    pointRadius: 0,
-                    parsing: {
-                        xAxisValue: "air_age",
-                        yAxisValue: "co2_ppm",
-                    }
-                }*/
                     
             ]
         })
@@ -179,14 +145,99 @@ const Visu2 = () => {
         fetchData();
     },[])
 
+    useEffect(() => {
+        const fetchData=async()=> {
+            const url_2 = "https://carbon-cruncher.azurewebsites.net/api/visu2/monthly";
+            const dataSet2 = [];
+        await fetch(url_2).then((monthlyCo2)=> {
+            console.log("Monthly data", monthlyCo2)
+            const res = monthlyCo2.json();
+            return res
+        }).then((res)=> {
+            console.log("result",res)
+            for (const val of res){
+                dataSet2.push(val.average);
+        }
+       
+        setMonthlyCo2 ({
+            labels: labels,   
+            datasets: [
+            
+                {
+                    label: "CO2 Monthly",
+                    data:dataSet2,
+                    borderColor: "rgb(240, 150, 15)",
+                    backgroundColor: "rgb(240, 150, 15)",
+                    pointRadius: 0,
+                    parsing: {
+                        xAxisValue: "Time",
+                        yAxisValue: "Average",
+                    }
+                },
+                    
+            ]
+        })
+        console.log("testData2", dataSet2)
+        }).catch(e => {
+            console.log("error", e)
+        })  
+        }
+        fetchData();
+    },[])
+
+    useEffect(() => {
+        const fetchData=async()=> {
+            const url_3 = "https://carbon-cruncher.azurewebsites.net/api/visu2/icecore";
+            const dataSet3 = [];
+        await fetch(url_3).then((icecore1)=> {
+            console.log("Ice core data", icecore1)
+            const res = icecore1.json();
+            return res
+        }).then((res)=> {
+            console.log("result",res)
+            for (const val of res){
+                dataSet3.push(val.co2Ppm);
+        }
+       
+        setIcecore1 ({
+            labels: labels2,   
+            datasets: [
+            
+                {
+                    label: "Ice Core 1",
+                    data:dataSet3,
+                    borderColor: "rgb(160, 60, 240)",
+                    backgroundColor: "rgb(160, 60, 240)",
+                    pointRadius: 0,
+                    parsing: {
+                        xAxisValue: "Time",
+                        yAxisValue: "Average",
+                    }
+                },
+                    
+            ]
+        })
+        console.log("testData3", dataSet3)
+        }).catch(e => {
+            console.log("error", e)
+        })  
+        }
+        fetchData();
+    },[])
+
+    /*const dataSet1 = _(annualCo2);
+    const dataSet2 = _(monthlyCo2);
+    const combinedData = _.mergeWith(dataSet1, dataSet2, (objValue, srcValue) => {
+        if (_.isArray(objValue)) {
+          return objValue.concat(srcValue);
+        }
+      });*/
+
+
     return (
     <div style={{width: "1000px", height: "500px"}}>
-        {
-            console.log("dataa", data)
-        }
-     <Line data={data} options={options}></Line>;
+        <h2>Atmospheric CO2 concentrations from Mauna Loa measurements starting 1958</h2>
+     <Line data={annualCo2} options={options}></Line>
     </div>
     )
 }
-
-export default Visu2;
--- front-end/carbon-cruncher/src/views/TempCO2View.js
+++ front-end/carbon-cruncher/src/views/TempCO2View.js
@@ -1,10 +1,11 @@
 import React from "react"
+import { Visu2 } from "../components/Visu2"
 
 export const TempCO2View = () => {
   return (
     <div className="tempCo2View">
-      <div>stuff</div>
-      TempCO2View
+      <div>Visualizations 1-3</div>
+      <Visu2/>
     </div>
   )
 }
