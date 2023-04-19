caf4a008b740bf74d4128dde83d532c020c911fe
Esa Salminen <t1saes01@students.oamk.fi>
2023-04-19 19:26:23 +0300

    added: user delete

--- front-end/carbon-cruncher/src/views/TempCO2View.js
+++ front-end/carbon-cruncher/src/views/TempCO2View.js
@@ -1,9 +1,11 @@
 import React from "react"
+import Visu1 from "../components/Visu1"
 import Visu3 from "../components/Visu3"
 
 export const TempCO2View = () => {
   return (
     <div className="tempCo2View">
+      <Visu1 />
       <Visu3 />
     </div>
   )
