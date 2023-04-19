4aa02f5455f4c71827605338dfbd2c5ba3dd2477
anssikulotie <anssi.kulotie@hotmail.com>
2023-04-19 20:30:37 +0300

    Revert "added: user delete"
    This reverts commit caf4a008b740bf74d4128dde83d532c020c911fe.

--- front-end/carbon-cruncher/src/views/TempCO2View.js
+++ front-end/carbon-cruncher/src/views/TempCO2View.js
@@ -1,12 +1,10 @@
 import React from "react"
-import Visu1 from "../components/Visu1"
 import Visu3 from "../components/Visu3"
 import Visu1 from "../components/Visu1"
 
 export const TempCO2View = () => {
   return (
     <div className="tempCo2View">
-      <Visu1 />
       <Visu3 />
       <Visu1 />
     </div>
