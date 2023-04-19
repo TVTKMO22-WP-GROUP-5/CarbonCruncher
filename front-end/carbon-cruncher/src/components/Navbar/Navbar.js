cd9a91e30a14fc22825782555596451dde9aa4b7
anssikulotie <anssi.kulotie@hotmail.com>
2023-04-19 20:34:25 +0300

    Revert "Revert "added: user delete""
    This reverts commit 4aa02f5455f4c71827605338dfbd2c5ba3dd2477.

--- front-end/carbon-cruncher/src/components/Navbar/Navbar.js
+++ front-end/carbon-cruncher/src/components/Navbar/Navbar.js
@@ -2,13 +2,23 @@ import React from "react"
 import styles from "./Navbar.module.css"
 import { Link } from "react-router-dom"
 import { AuthContext } from "../AuthProvider"
+import { imageAssets } from "../../assets/Assets"
 
 const Navbar = () => {
-  const { onLogout, user } = React.useContext(AuthContext)
+  const { onLogout, onDelete, user } = React.useContext(AuthContext)
 
   return (
     <div className={styles.container}>
-      <p id="loggedUser">User: {user}</p>
+      <p id="loggedUser" className={styles.user}>
+        {user}
+        <img
+          src={imageAssets.icon.deleteIcon}
+          alt="delete"
+          id="deleteUser"
+          className={styles.delete}
+          onClick={onDelete}
+        ></img>
+      </p>
       <div className={styles.linkContainer}>
         <Link className={styles.link} to="/tempco2">
           Temperature & CO2 Data
